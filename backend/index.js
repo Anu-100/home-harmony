b7ada48e82df17625aeeeb94144f4368a4277b00
ANUSREE <anusreemitra47@gmail.com>
2023-11-10 18:19:32 +0530

    database connection

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,18 @@
 import express from 'express'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 const app = express()
+
+mongoose.connect(process.env.MONGO_URL)
+    .then(() => {
+        console.log("Database connected successfully")
+    })
+    .catch((err) => {
+        console.log(err)
+    })
 
 app.listen(5000, () => {
     console.log("Running on port 5000...")
