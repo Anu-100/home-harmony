907dafe1de81269175655bdae0b6ceeb9f585c11
ANUSREE <anusreemitra47@gmail.com>
2023-11-11 13:16:54 +0530

    sign up api route

---
+++ backend/controllers/user_controller.js
@@ -0,0 +1,5 @@
+const test = (req, res) => {
+    res.json({message: "Happy Coding..."})
+}
+
+export default test
--- backend/index.js
+++ backend/index.js
@@ -2,9 +2,14 @@ import express from 'express'
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
 
+import userRouter from './routes/user_router.js'
+import authRouter from './routes/auth_router.js'
+
 dotenv.config()
 
 const app = express()
+
+app.use(express.json())
 
 mongoose.connect(process.env.MONGO_URL)
     .then(() => {
@@ -13,6 +18,9 @@ mongoose.connect(process.env.MONGO_URL)
     .catch((err) => {
         console.log(err)
     })
+
+app.use("/api/users", userRouter)
+app.use("/api/auth", authRouter)
 
 app.listen(5000, () => {
     console.log("Running on port 5000...")
