7f1bad1b7afad74ed0387822e5faab8657ce652b
ANUSREE <anusreemitra47@gmail.com>
2023-11-17 12:40:14 +0530

    created profile page UI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
-import Home from './pages/Home'
-import SignIn from './pages/SignIn'
-import SignUp from './pages/SignUp'
-import About from './pages/About'
-import Profile from './pages/Profile'
-import Header from './components/Header'
-import PrivateRoute from './components/PrivateRoute'
+import Home from './pages/Home.jsx'
+import SignIn from './pages/SignIn.jsx'
+import SignUp from './pages/SignUp.jsx'
+import About from './pages/About.jsx'
+import Profile from './pages/Profile.jsx'
+import Header from './components/Header.jsx'
+import PrivateRoute from './components/PrivateRoute.jsx'
 
 export default function App() {
   return (
--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 
 const Profile = () => {
+  const { currentUser } = useSelector(state => state.user)
   return (
-    <h1>Profile</h1>
+    <div className='mx-auto max-w-lg p-3'>
+      <h1 className='text-3xl text-center font-semibold my-7'>Profile</h1>
+      <form className='flex flex-col gap-4'>
+        <img className='rounded-full h-20 w-20 self-center object-cover mt-3' src={currentUser.avatar} alt='profile' />
+        <input type='text' placeholder='Username' id='Username' className='border rounded-lg p-3 focus:outline-none'/>
+        <input type='email' placeholder='Email' id='Email' className='border rounded-lg p-3 focus:outline-none'/>
+        <button className='bg-blue-800 text-white p-3 rounded-lg'>Update</button>
+      </form>
+      <div className='flex justify-between mt-5'>
+        <span className='text-red-700'>Delete Account</span>
+        <span className='text-blue-800'>Sign Out</span>
+      </div>
+    </div>
   )
 }
 
