afb47e7a52a9df57116de83be56d31377426ee38
ANUSREE <anusreemitra47@gmail.com>
2023-11-27 12:17:28 +0530

    Created the backend for getting an user's listings

--- backend/controllers/user_controller.js
+++ backend/controllers/user_controller.js
@@ -1,40 +1,57 @@
-import bcryptjs from 'bcryptjs'
-import error_handler from "../utils/error.js"
-import User from '../models/user_model.js'
+import bcryptjs from "bcryptjs";
+import error_handler from "../utils/error.js";
+import User from "../models/user_model.js";
+import Listing from "../models/listing_model.js";
 
 export const test = (req, res) => {
-    res.json({message: "Happy Coding..."})
-}
+  res.json({ message: "Happy Coding..." });
+};
 
 export const updateUserInfo = async (req, res, next) => {
-    if(req.user.id !== req.params.id) return next(error_handler(401, "You can only update your own account!"))
-    try {
-        if(req.body.password){
-            req.body.password = bcryptjs.hashSync(req.body.password, 10)
-        }
-        const updatedUser = await User.findByIdAndUpdate(req.params.id, {
-            $set: {
-                username: req.body.username,
-                email: req.body.email,
-                avatar: req.body.avatar
-            }
-        }, {new: true})
-        const { password, ...other_info } = updatedUser._doc
-        res.status(200).json(other_info)
-    } catch (error) {
-        next(error)
+  if (req.user.id !== req.params.id)
+    return next(error_handler(401, "You can only update your own account!"));
+  try {
+    if (req.body.password) {
+      req.body.password = bcryptjs.hashSync(req.body.password, 10);
     }
-}
+    const updatedUser = await User.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: {
+          username: req.body.username,
+          email: req.body.email,
+          avatar: req.body.avatar,
+        },
+      },
+      { new: true }
+    );
+    const { password, ...other_info } = updatedUser._doc;
+    res.status(200).json(other_info);
+  } catch (error) {
+    next(error);
+  }
+};
 
 export const deleteUserInfo = async (req, res, next) => {
-    if(req.user.id !== req.params.id) {
-        return next(error_handler(401, "You can only delete your own account!"))
-    }
+  if (req.user.id !== req.params.id) {
+    return next(error_handler(401, "You can only delete your own account!"));
+  }
+  try {
+    await User.findByIdAndDelete(req.params.id);
+    res.clearCookie("access_token");
+    res.status(200).json("User has been deleted successfully!");
+  } catch (error) {}
+};
+
+export const getUserListings = async (req, res, next) => {
+  if (req.user.id === req.params.id) {
     try {
-        await User.findByIdAndDelete(req.params.id)
-        res.clearCookie('access_token')
-        res.status(200).json("User has been deleted successfully!")
+      const listings = await Listing.find({ userRef: req.params.id });
+      res.status(200).json(listings);
     } catch (error) {
-        
+      next(error);
     }
-}
+  } else {
+    return next(error_handler(401, "You are unauthorized!"));
+  }
+};
--- backend/routes/user_router.js
+++ backend/routes/user_router.js
@@ -1,11 +1,18 @@
-import express from 'express'
-import { deleteUserInfo, test, updateUserInfo } from '../controllers/user_controller.js'
-import { verifyToken } from '../utils/verifyUser.js'
+import express from "express";
+import {
+  deleteUserInfo,
+  test,
+  updateUserInfo,
+  getUserListings,
+} from "../controllers/user_controller.js";
+import { verifyToken } from "../utils/verifyUser.js";
 
-const router = express.Router()
+const router = express.Router();
 
-router.get("/test", test)
-router.put("/update/:id", verifyToken, updateUserInfo)
-router.delete("/delete/:id", verifyToken, deleteUserInfo)
+router.get("/test", test);
+router.put("/update/:id", verifyToken, updateUserInfo);
+router.delete("/delete/:id", verifyToken, deleteUserInfo);
 
-export default router
+router.get("/user-listings/:id", verifyToken, getUserListings);
+
+export default router;
