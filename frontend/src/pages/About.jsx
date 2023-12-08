50dc348d244b8d58fd39c11098cacb0353e945b6
ANUSREE <anusreemitra47@gmail.com>
2023-12-08 21:35:33 +0530

    Working on the About page.

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -2,8 +2,28 @@ import React from "react";
 
 const About = () => {
   return (
-    <div className="max-w-6xl mx-auto">
-      <h1 className="font-bold text-2xl sm:text-5xl">About Home Harmony</h1>
+    <div className="max-w-6xl mx-auto px-4 py-20">
+      <h1 className="font-bold text-2xl sm:text-5xl mb-4">About Home Harmony</h1>
+      <p className="mb-4 text-slate-700 font-semibold">
+        Home Harmony is a leading real estate agency that specializes in helping
+        clients buy, sell and rent properties in the most desirable
+        neighborhoods. Our team of experienced agents is dedicated to providing
+        exceptional service and making the buying and selling process as smooth
+        as possible.
+      </p>
+      <p className="mb-4 text-slate-700 font-semibold">
+        Our mission is to help our clients achieve their real estate goals by
+        providing expert advice, personalized service and a deep understanding
+        of the local market. Whether you are looking to buy, sell or rent a
+        property, we are here to help you every step of the way.
+      </p>
+      <p className="mb-4 text-slate-700 font-semibold">
+        Our team of agents has plenty of experience and knowledge in the real
+        estate industry and we are highly committed to providing highest level
+        of service to our clients. We believe that buying and selling a property
+        should be rewarding experience and we are dedicated to make that happen
+        to each and every one of our client.
+      </p>
     </div>
   );
 };
